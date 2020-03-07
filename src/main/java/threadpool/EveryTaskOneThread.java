817411e778eed9cf561292146e1d7ec258c32c22
mkwfqd <maoturing@qq.com>
2020-03-07 22:50:30 +0800

    Initial commit

---
+++ src/main/java/threadpool/EveryTaskOneThread.java
@@ -0,0 +1,22 @@
+package threadpool;
+
+/**
+ * 每个任务都创建一个新的线程来执行
+ */
+public class EveryTaskOneThread {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            System.out.println("执行了任务...");
+        };
+
+        // 1. 执行一个任务
+        Thread thread = new Thread(task);
+        thread.start();
+
+        // 2. 执行1000个任务
+        for (int i = 0; i < 100000; i++) {
+            Thread thread1 = new Thread(task);
+            thread1.start();
+        }
+    }
+}
