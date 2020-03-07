9e377b7da00409c0f6e6d2793d2b92f9f4a6bcc4
mkwfqd <maoturing@qq.com>
2020-03-07 22:51:54 +0800

    + day1:线程池

--- src/main/java/threadpool/EveryTaskOneThread.java
+++ src/main/java/threadpool/EveryTaskOneThread.java
@@ -5,16 +5,14 @@ package threadpool;
  */
 public class EveryTaskOneThread {
     public static void main(String[] args) {
-        Runnable task = () -> {
-            System.out.println("执行了任务...");
-        };
+        Runnable task = () -> System.out.println("执行了任务...");
 
         // 1. 执行一个任务
         Thread thread = new Thread(task);
         thread.start();
 
         // 2. 执行1000个任务
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000; i++) {
             Thread thread1 = new Thread(task);
             thread1.start();
         }
